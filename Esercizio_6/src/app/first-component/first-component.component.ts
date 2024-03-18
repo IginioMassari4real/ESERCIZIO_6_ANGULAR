ff4c466895b181f90b5abb835ecd5eef79bb2e3a
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-03-18 09:14:58 +0000

    ho inserito la classifica dei giocatori nell'array

--- Esercizio_6/src/app/first-component/first-component.component.ts
+++ Esercizio_6/src/app/first-component/first-component.component.ts
@@ -11,25 +11,25 @@ export class FirstComponentComponent {
   
   calciatori = [
 
-    {nome: "Kylian", cognome: "Mbappè", età:25, colore:"lightgreen", nazionalità: "francia",ruolo: "attaccante", overall:91,immagine:"../../assets/antonino_.jpg"  },
- 
-    {nome: "Kylian", cognome: "Mbappè", età:25, colore:"lightgreen", nazionalità: "francia",ruolo: "attaccante", overall:91,immagine:"../../assets/antonino_.jpg"  },
-
     {nome: "Kylian", cognome: "Mbappè", età:25, colore:"lightgreen", nazionalità: "francia",ruolo: "attaccante", overall:91, immagine:"../../assets/antonino_.jpg"  },
 
-    {nome: "Kevin", cognome: "De Bruyne", età:32, colore:"lightgreen", nazionalità: "belgio",ruolo: "centrocampista", immagine:"../../assets/antonino_.jpg"  },
+    {nome: "Kevin", cognome: "De Bruyne", età:32, colore:"lightgreen", nazionalità: "belgio",ruolo: "centrocampista", overall:91,immagine:"../../assets/antonino_.jpg"  },
 
-    {nome: "Erling", cognome: "Haaland", età:23, colore:"lightgreen", nazionalità: "norvegia",ruolo: "attaccante", immagine:"../../assets/antonino_.jpg"  },
+    {nome: "Erling", cognome: "Haaland", età:23, colore:"lightgreen", nazionalità: "norvegia",ruolo: "attaccante", overall:91,immagine:"../../assets/antonino_.jpg"  },
 
-    {nome: "Alexia", cognome: "Putellas", età:30, colore:"lightgreen", nazionalità: "spagna",ruolo: "attaccante", immagine:"../../assets/antonino_.jpg"  },
+    {nome: "Alexia", cognome: "Putellas", età:30, colore:"lightgreen", nazionalità: "spagna",ruolo: "attaccante", overall:91,immagine:"../../assets/antonino_.jpg"  },
 
-    {nome: "Kylian", cognome: "Mbappè", età:25, colore:"lightgreen", nazionalità: "francia",ruolo: "attaccante", immagine:"../../assets/antonino_.jpg"  },
+    {nome: "Lionel", cognome: "Messi", età:36, colore:"lightgreen", nazionalità: "argentina",ruolo: "attaccante", overall:90,immagine:"../../assets/antonino_.jpg"  },
 
-    {nome: "Kylian", cognome: "Mbappè", età:25, colore:"lightgreen", nazionalità: "francia",ruolo: "attaccante", immagine:"../../assets/antonino_.jpg"  },
+    {nome: "Sam", cognome: "Kerr", età:30, colore:"lightgreen", nazionalità: "australia",ruolo: "attaccante", overall:90,immagine:"../../assets/antonino_.jpg"  },
 
-    {nome: "Kylian", cognome: "Mbappè", età:25, colore:"lightgreen", nazionalità: "francia",ruolo: "attaccante", immagine:"../../assets/antonino_.jpg"  },
+    {nome: "Karim", cognome: "Benzema", età:36, colore:"lightgreen", nazionalità: "francia",ruolo: "attaccante", overall:90,immagine:"../../assets/antonino_.jpg"  },
 
-    {nome: "Kylian", cognome: "Mbappè", età:25, colore:"lightgreen", nazionalità: "francia",ruolo: "attaccante", immagine:"../../assets/antonino_.jpg"  },
+    {nome: "Caroine Graham", cognome: "Hansen", età:33, colore:"lightgreen", nazionalità: "norvegia",ruolo: "attaccante", overall:90,immagine:"../../assets/antonino_.jpg"  },
+
+    {nome: "Thibaut", cognome: "Courtois", età:31, colore:"lightgreen", nazionalità: "belgio",ruolo: "portiere", overall:90,immagine:"../../assets/antonino_.jpg"  },
+ 
+    {nome: "Aitana", cognome: "Bonmati'", età:26, colore:"lightgreen", nazionalità: "spagna",ruolo: "attaccante", overall:90,immagine:"../../assets/antonino_.jpg"  },
   ]
 
 }
